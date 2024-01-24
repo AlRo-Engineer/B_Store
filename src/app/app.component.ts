41f37e15c43096517e31b72015244c02fd34a88a
AlRo-Engineer <fastova.engineer@gmail.com>
2024-01-24 14:49:26 -0500

    media query add and loader set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {AppService} from "./app.service";
 
@@ -9,6 +9,12 @@ import {AppService} from "./app.service";
 })
 export class AppComponent {
   currency = '$';
+  loaderShowed = true;
+  loader = true;
+
+  orderImageStyle: any;
+  mainImageStyle: any;
+
   form = this.fb.group({
     order: ['', Validators.required],
     name: ['', Validators.required],
@@ -20,11 +26,26 @@ export class AppComponent {
   constructor(private fb: FormBuilder, private appService: AppService) {
   }
 
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.orderImageStyle = {transform: 'translate(-' + ((e.clientX * 0.3) / 8) + 'px, -' + ((e.clientY * 0.3) / 8) + 'px)'};
+    this.mainImageStyle = {transform: 'translate(-' + ((e.clientX * 0.3) / 8) + 'px, -' + ((e.clientY * 0.3) / 8) + 'px)'};
+  }
+
   ngOnInit() {
+    setTimeout(() => {
+      this.loaderShowed = false;
+    }, 3000);
+    setTimeout(() => {
+      this.loader = false;
+    }, 4000);
     this.appService.getData().subscribe(data => this.productsData = data);
   }
 
-  scrollTo(target: HTMLElement, burger?: any) {
+  scrollTo(target
+             :
+             HTMLElement, burger ?: any
+  ) {
     target.scrollIntoView({behavior: 'smooth'});
     if (burger) {
       this.form.patchValue({order: burger.title + ' (' + burger.price + ' ' + this.currency + ')'});
